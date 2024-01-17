d2819df82f75812ebb1e150a1565fa32a9716ddd
Uche Ozoemena <codewithoz@outlook.com>
2024-01-17 19:38:33 +0100

    Initialize pinecone

--- src/datasets/datasets.py
+++ src/datasets/datasets.py
@@ -1,7 +1,17 @@
-from pinecone_datasets import load_dataset, list_datasets
+import os
+import pinecone
+from dotenv import load_dotenv
+from pinecone_datasets import load_dataset
+
+load_dotenv()
+
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
 dataset = load_dataset("youtube-transcripts-text-embedding-ada-002")
 
 # drop the sparse values as they are not needed for this example
-dataset.documents.drop(["metadata"], axis=1, inplace=True)
+dataset.documents.drop(["metadata", "sparse_values"], axis=1, inplace=True)
 dataset.documents.rename(columns={"blob": "metadata"}, inplace=True)
+
+# initialize pinecone
+pinecone.init(api_key=pinecone_api_key)
